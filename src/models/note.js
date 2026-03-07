e9d2317f8c10eb9357da31a1312a65bd25bcccc8
Crazy_Fox <holmyliszag@ukr.net>
2026-03-07 17:40:58 +0200

    fix index try-3

--- src/models/note.js
+++ src/models/note.js
@@ -30,13 +30,13 @@ const noteShema = new Schema(
   },
 );
 
-noteShema.index(
+noteShema.createIndex(
   { title: 'text', content: 'text' },
-  // {
-  //   name: 'NotesTextIndex',
-  //   weights: { title: 5, content: 1 },
-  //   default_language: 'english',
-  // },
+  {
+    name: 'NotesTextIndex',
+    weights: { title: 5, content: 1 },
+    default_language: 'english',
+  },
 );
 
 export const Note = model('Note', noteShema);
